a9b6545bdf0087b27c3b3efe083cfca883ab2758
Jesse Kalil Martins <jessekalyl1491@gmail.com>
2021-06-03 22:00:10 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="@assets/logo.png" />
+  
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  background: #21222c;
+  height: 100vh;
+  width: 100vw;
+}
+</style>
